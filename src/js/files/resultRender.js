1f320ce972e0dff23fbf77a65878267f3a9a921f
Scarbor0 <Yurachapyuk@gmail.com>
2023-03-27 20:33:14 +0300

    final

--- src/js/files/resultRender.js
+++ src/js/files/resultRender.js
@@ -27,8 +27,10 @@ export function resultRenderHtml(selector, options) {
 	if (options.isError === true) {
 		$el.insertAdjacentHTML('beforeend', `
 		<div class="result-render-dbd error-blinking-result unselectable">
-			<h4 class='tittle tittle-error'>Помилка!!!</h4>
-			<p>${options.errorMessage}</p>
+			<h4 class='tittle tittle-error tittle-result'>Помилка!!!</h4>
+			<div class="dbd__column-result">
+				<p class="dbd__error-result">${options.errorMessage}</p>
+			</div>
 		</div>
 		`)
 		$el.classList.add(options.errorClassName)
@@ -36,25 +38,68 @@ export function resultRenderHtml(selector, options) {
 		$el.classList.remove(options.errorClassName)
 		$el.insertAdjacentHTML('beforeend', `
 			<div class="result-render-dbd">
-				<h4 class="tittle dbd__tittle">Результат</h4>
-				<table class="dbd-table">
-					<tbody>
-						<tr>
-							<th>Період&nbsp;${incldFirstDayRender()}:</th>
-							<td>${options.starDate} - ${currentOrSetEndDate}</td>
-						</tr>
-						<tr>
-							<th>Інтервал:</th>
-							<td>${options.years}р. ${options.months}міс. ${options.days}дн.</td>
-						</tr>
-						<tr>
-							<th>Календарних днів:</th>
-							<td>${options.totalDays} дн.</td>
-						</tr>
-					</tbody>
-				</table>
+				<h4 class="tittle dbd__tittle tittle-result">Результат</h4>
+				<div class="dbd__table">
+					<div class="dbd__title-result-wrapper">
+							<div class="dbd__column-title">
+								<p>Період&nbsp;${incldFirstDayRender()}:</p>
+							</div>
+							<div class="dbd__column-result">
+								<p>${options.starDate} - ${currentOrSetEndDate}</p>
+							</div>
+					</div>
+					<div class="dbd__title-result-wrapper">
+						<div class="dbd__column-title">
+							<p>Інтервал:</p>
+						</div>
+						<div class="dbd__column-result">
+							<p>${options.years}р. ${options.months}міс. ${options.days}дн.</p>
+						</div>
+					</div>
+					<div class="dbd__title-result-wrapper">
+						<div class="dbd__column-title">
+							<p>Календарних&nbsp;днів:</p>
+						</div>
+						<div class="dbd__column-result">
+							<p>${options.totalDays} дн.</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		`)
 
 	}
+	// if (options.isError === true) {
+	// 	$el.insertAdjacentHTML('beforeend', `
+	// 	<div class="result-render-dbd error-blinking-result unselectable">
+	// 		<h4 class='tittle tittle-error'>Помилка!!!</h4>
+	// 		<p>${options.errorMessage}</p>
+	// 	</div>
+	// 	`)
+	// 	$el.classList.add(options.errorClassName)
+	// } else if (options.isError === false) {
+	// 	$el.classList.remove(options.errorClassName)
+	// 	$el.insertAdjacentHTML('beforeend', `
+	// 		<div class="result-render-dbd">
+	// 			<h4 class="tittle dbd__tittle">Результат</h4>
+	// 			<table class="dbd-table">
+	// 				<tbody>
+	// 					<tr>
+	// 						<th>Період&nbsp;${incldFirstDayRender()}:</th>
+	// 						<td>${options.starDate} - ${currentOrSetEndDate}</td>
+	// 					</tr>
+	// 					<tr>
+	// 						<th>Інтервал:</th>
+	// 						<td>${options.years}р. ${options.months}міс. ${options.days}дн.</td>
+	// 					</tr>
+	// 					<tr>
+	// 						<th>Календарних днів:</th>
+	// 						<td>${options.totalDays} дн.</td>
+	// 					</tr>
+	// 				</tbody>
+	// 			</table>
+	// 		</div>
+	// 	`)
+
+	// }
 }
